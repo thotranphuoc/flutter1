5c5244349fe9e5f59b577d1e91d04d1b567c6e90
mac <mac@Jade-MBP.net.fpt>
2018-11-17 00:56:51 +0700

    v0.0.4: create array of string, add function to add item to array

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,20 @@ import 'package:flutter/material.dart';
 // main doest return anything
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   // it tells that build function override all from StatelessWidget
   // build() return a widget
   // input of build() should be a BuildContext
+
+  List<String> _products = ['Food Tester'];
   @override
   Widget build(BuildContext contect) {
     return MaterialApp(
@@ -21,7 +31,10 @@ class MyApp extends StatelessWidget {
             Container(
               margin: EdgeInsets.all(10.0),
               child: RaisedButton(
-                onPressed: () {},
+                onPressed: () {
+                  _products.add('New Food Tester');
+                  print(_products);
+                },
                 child: Text('Add New'),
               ),
             ),
