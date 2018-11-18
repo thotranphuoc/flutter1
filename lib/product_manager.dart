88db0f41a65941b351c7cf18f7ad853d5e551014
mac <mac@Jade-MBP.local>
2018-11-18 18:16:21 +0700

    v0.0.7: add some lifecyclehooks

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -4,10 +4,13 @@ import './products.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
-  ProductManager(this.startingProduct);
+  ProductManager(this.startingProduct){
+    print('[ProductManager] constructor()');
+  }
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
+    print('[ProductManager] createState()');
     return _ProductManagerState();
   }
 }
@@ -17,13 +20,22 @@ class _ProductManagerState extends State<ProductManager> {
   @override
   void initState() {
     // TODO: implement initState
+    print('[ProductManagerState] initState()');
     _products.add(widget.startingProduct);
     super.initState();
   }
 
   @override
+    void didUpdateWidget(ProductManager oldWidget) {
+      // TODO: implement didUpdateWidget
+      print('[ProductManagerState] didUpdateWidget()');
+      super.didUpdateWidget(oldWidget);
+    }
+
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    print('[ProductManagerState] build()');
     return Column(
       children: <Widget>[
         Container(
--- lib/products.dart
+++ lib/products.dart
@@ -4,17 +4,20 @@ class Products extends StatelessWidget {
   final List<String> products;
 
   // get the input and assign to products
-  Products(this.products);
+  Products(this.products){
+    print('product.dart constructor()');
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    print('product.dart build()');
     return Column(
       children: products
           .map((element) => Card(
                 child: Column(
                   children: <Widget>[
-                    Image.asset('assets/dalat.jpg'),
+                    // Image.asset('assets/dalat.jpg'),
                     Text(element)
                   ],
                 ),
