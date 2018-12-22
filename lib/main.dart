c04e7eece11e19a769e192b5e1b95335f57a8919
mac <mac@Jade-MBP.local>
2018-12-22 14:45:50 +0700

    expended view, occupy all remaining space on screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import './product_manager.dart';
 
-
 // void main() => runApp(MyApp());
 void main() {
   runApp(MyApp());
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowMaterialGrid: true,
+      debugShowMaterialGrid: false,
       theme: ThemeData(
           brightness: Brightness.light,
           primarySwatch: Colors.deepOrange,
