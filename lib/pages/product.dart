dc60cf1b3ed9cf2b841cf2d6e38c76b36489a3a7
mac <mac@Jade-MBP.net.fpt>
2018-12-24 23:36:19 +0700

    solve error with phycal button back, or navigator back button -> wrap them into WillPopScope widget

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+
 // import '../product_manager.dart';
 
 class ProductPage extends StatelessWidget {
@@ -9,19 +11,26 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Details'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Text(title),
-            RaisedButton(
-              child: Text('DELETE'),
-              onPressed: () => Navigator.pop(context, true),
-            )
-          ],
-        ));
+    return WillPopScope(
+      onWillPop: () {
+        print('Back button pressed');
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text('Details'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Text(title),
+              RaisedButton(
+                child: Text('DELETE'),
+                onPressed: () => Navigator.pop(context, true),
+              )
+            ],
+          )),
+    );
   }
 }
