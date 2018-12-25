04e0e0b3670e511141a77916917a6fe3ab7c101c
mac <mac@Jade-MBP.net.fpt>
2018-12-25 08:39:02 +0700

    add login page, pushReplacement()

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import './pages/home.dart';
+import './pages/auth.dart';
 
 // void main() => runApp(MyApp());
 void main() {
@@ -19,6 +19,6 @@ class MyApp extends StatelessWidget {
             brightness: Brightness.light,
             primarySwatch: Colors.deepOrange,
             accentColor: Colors.deepPurple),
-        home: HomePage());
+        home: AuthPage());
   }
 }
