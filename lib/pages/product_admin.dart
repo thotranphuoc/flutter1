4e3fa9902c05fdd56396309aa530bb7889c17e16
mac <mac@Jade-MBP.local>
2018-12-25 18:02:43 +0700

    add tabs bar

--- lib/pages/product_admin.dart
+++ lib/pages/product_admin.dart
@@ -5,37 +5,48 @@ class ProductAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-        drawer: Drawer(
-          child: Column(
-            children: <Widget>[
-              AppBar(
-                automaticallyImplyLeading: false,
-                title: Text('Choose'),
-              ),
-              ListTile(
-                title: Text('All Products'),
-                onTap: () {
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => ProductsPage()));
-                },
-              )
-            ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          drawer: Drawer(
+            child: Column(
+              children: <Widget>[
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  title: Text('Choose'),
+                ),
+                ListTile(
+                  title: Text('All Products'),
+                  onTap: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => ProductsPage()));
+                  },
+                )
+              ],
+            ),
           ),
-        ),
-        appBar: AppBar(
-          title: Text('Product Manager'),
-        ),
-        body: Center(
-            child: RaisedButton(
-                child: Text('Some product here'),
-                onPressed: () {
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => ProductsPage()));
-                })));
+          appBar: AppBar(
+              title: Text('Product Manager'),
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(
+                    icon: Icon(Icons.create),
+                    text: 'Create Product',
+                  ),
+                  Tab(icon: Icon(Icons.list), text: 'My Products')
+                ],
+              )),
+          body: Center(
+              child: RaisedButton(
+                  child: Text('Some product here'),
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => ProductsPage()));
+                  }))),
+    );
   }
 }
