6951ebf081adfed03fd106115c8e11677d4e0239
mac <mac@Jade-MBP.net.fpt>
2018-12-26 08:55:19 +0700

    convert stateless to stateful widget in order to have setState() to update value of var textFieldValue

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String textFieldValue = '';
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -18,10 +27,18 @@ class ProductCreatePage extends StatelessWidget {
     //   },
     // ));
 
-    return Column(children: <Widget>[
-      TextField(onChanged: (value){
-        print(value);
-      },)
-    ],);
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            print(value);
+            setState(() {
+              textFieldValue = value;
+            });
+          },
+        ),
+        Text(textFieldValue)
+      ],
+    );
   }
 }
