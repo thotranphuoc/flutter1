d823f4c19188e1d6f92a2c911d807b02b926d0f3
mac <mac@Jade-MBP.net.fpt>
2018-12-29 00:23:14 +0700

    add background image, center with SingleChildScrollView, fillColor

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -21,51 +21,65 @@ class _AuthPageState extends State<AuthPage> {
         title: Text('Login'),
       ),
       body: Container(
-        margin: EdgeInsets.all(10.0),
-        child: ListView(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (String value) {
-                print(value);
-                setState(() {
-                  _emailValue = value;
-                });
-              },
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              onChanged: (String value) {
-                print(value);
-                setState(() {
-                  _passValue = value;
-                });
-              },
-            ),
-            SwitchListTile(
-              value: _acceptTerms,
-              onChanged: (bool newvalue) {
-                setState(() {
-                  _acceptTerms = newvalue;
-                });
-              },
-              title: Text('Accept terms'),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-              child: Text('Login'),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/products');
-              },
-            )
-          ],
-        ),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.5), BlendMode.dstATop),
+                image: AssetImage('assets/background.jpg'))),
+        padding: EdgeInsets.all(10.0),
+        child: Center(
+            child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                    labelText: 'Email', filled: true, fillColor: Colors.white),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (String value) {
+                  print(value);
+                  setState(() {
+                    _emailValue = value;
+                  });
+                },
+              ),
+              SizedBox(height: 10.0,),
+              TextField(
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    filled: true,
+                    fillColor: Colors.white),
+                obscureText: true,
+                onChanged: (String value) {
+                  print(value);
+                  setState(() {
+                    _passValue = value;
+                  });
+                },
+              ),
+              SwitchListTile(
+                value: _acceptTerms,
+                onChanged: (bool newvalue) {
+                  setState(() {
+                    _acceptTerms = newvalue;
+                  });
+                },
+                title: Text('Accept terms'),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                child: Text('Login'),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/products');
+                },
+              )
+            ],
+          ),
+        )),
       ),
       // body: Center(
       //     child: RaisedButton(
