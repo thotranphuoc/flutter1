b459866db5b2429652a09e53e35896da20ef4fa2
mac <mac@Jade-MBP.net.fpt>
2018-12-29 19:00:13 +0700

    understand MediaQuery with ListView

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -16,9 +16,13 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   double _priceValue;
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 550.0 ? 500 : deviceWidth * 0.95;
+    final double targetPadding = deviceWidth - targetWidth;
     return Container(
       margin: EdgeInsets.all(10.0),
       child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
         children: <Widget>[
           _buildTitleTextField(),
           _buildDescriptionTextField(),
