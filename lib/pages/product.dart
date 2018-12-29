f96870106395fa14706d5c299f3e259f007d0584
mac <mac@Jade-MBP.net.fpt>
2018-12-29 15:15:39 +0700

    move code out to AddressTag

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import '../widgets/products/price_tag.dart';
-// import '../product_manager.dart';
+import '../widgets/ui_elements/title_default.dart';
 
 class ProductPage extends StatelessWidget {
   final String title;
@@ -28,11 +28,12 @@ class ProductPage extends StatelessWidget {
               Image.asset(imageUrl),
               Container(
                 padding: EdgeInsets.all(10.0),
-                child: Text(title,
-                    style: TextStyle(
-                        fontSize: 26.0,
-                        fontFamily: 'Oswald',
-                        fontWeight: FontWeight.bold)),
+                child: TitleDefault(title),
+                // Text(title,
+                //     style: TextStyle(
+                //         fontSize: 26.0,
+                //         fontFamily: 'Oswald',
+                //         fontWeight: FontWeight.bold)),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './price_tag.dart';
+import '../ui_elements/title_default.dart';
+import './address_tag.dart';
 
 class ProductCard extends StatelessWidget {
   final Map<String, dynamic> product;
@@ -18,26 +20,14 @@ class ProductCard extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(
-                    product['title'],
-                    style: TextStyle(
-                        fontSize: 26.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Oswald'),
-                  ),
+                  TitleDefault(product['title']),
                   SizedBox(
                     width: 20.0,
                   ),
                   PriceTag(product['price'].toString()),
                 ],
               )),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 1.0),
-                borderRadius: BorderRadius.circular(4.0)),
-            child: Text('Union Square, San Francisco'),
-          ),
+          AddressTag('Union Square, San Francisco'),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
