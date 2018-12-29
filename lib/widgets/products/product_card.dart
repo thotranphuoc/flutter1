584dcc6f5a81f998f4a09635f0b7ae0b6161d268
mac <mac@Jade-MBP.net.fpt>
2018-12-29 14:07:39 +0700

    extract code of product into seperated widget

---
+++ lib/widgets/products/product_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import './price_tag.dart';
+
+class ProductCard extends StatelessWidget {
+  final Map<String, dynamic> product;
+  final int productIndex;
+
+  ProductCard(this.product, this.productIndex);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/dalat.jpg'),
+          Container(
+              padding: EdgeInsets.only(top: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    product['title'],
+                    style: TextStyle(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Oswald'),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  PriceTag(product['price'].toString()),
+                ],
+              )),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                borderRadius: BorderRadius.circular(4.0)),
+            child: Text('Union Square, San Francisco'),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.info),
+                  color: Theme.of(context).accentColor,
+                  onPressed: () => Navigator.pushNamed(
+                      context, '/product/' + productIndex.toString())
+                  //     .then((onValue) {
+                  //   if (onValue) {
+                  //     deleteProduct(index);
+                  //   }
+                  // }),
+                  ),
+              IconButton(
+                  icon: Icon(Icons.favorite_border),
+                  color: Colors.red,
+                  onPressed: () => Navigator.pushNamed(
+                      context, '/product/' + productIndex.toString())
+                  //     .then((onValue) {
+                  //   if (onValue) {
+                  //     deleteProduct(index);
+                  //   }
+                  // }),
+                  )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/products/products.dart
+++ lib/widgets/products/products.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
-import '../../pages/product.dart';
-import './price_tag.dart';
+import './product_card.dart';
 
 class Products extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   // final Function deleteProduct;
   // products: required, deleteProduct: optional
-  Products(
-    this.products,
-    //  {this.deleteProduct}
-  ) {
-    print('product.dart constructor()');
-  }
+  Products(this.products);
 
   @override
   Widget build(BuildContext context) {
@@ -20,65 +14,7 @@ class Products extends StatelessWidget {
   }
 
   Widget _buildProductItem(BuildContext context, int index) {
-    return Card(
-      child: Column(
-        children: <Widget>[
-          Image.asset('assets/dalat.jpg'),
-          Container(
-              padding: EdgeInsets.only(top: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    products[index]['title'],
-                    style: TextStyle(
-                        fontSize: 26.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Oswald'),
-                  ),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  PriceTag(products[index]['price'].toString()),
-                ],
-              )),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 1.0),
-                borderRadius: BorderRadius.circular(4.0)),
-            child: Text('Union Square, San Francisco'),
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                  icon: Icon(Icons.info),
-                  color: Theme.of(context).accentColor,
-                  onPressed: () => Navigator.pushNamed(
-                      context, '/product/' + index.toString())
-                  //     .then((onValue) {
-                  //   if (onValue) {
-                  //     deleteProduct(index);
-                  //   }
-                  // }),
-                  ),
-              IconButton(
-                  icon: Icon(Icons.favorite_border),
-                  color: Colors.red,
-                  onPressed: () => Navigator.pushNamed(
-                      context, '/product/' + index.toString())
-                  //     .then((onValue) {
-                  //   if (onValue) {
-                  //     deleteProduct(index);
-                  //   }
-                  // }),
-                  )
-            ],
-          )
-        ],
-      ),
-    );
+    return ProductCard(products[index], index);
   }
 
   Widget _buildProductList() {
@@ -89,10 +25,6 @@ class Products extends StatelessWidget {
         itemCount: products.length,
       );
     } else {
-      // productListWidget = Center(
-      //   child: Text('No products found, please add some'),
-      // );
-
       productListWidget =
           Container(); // could be an empty container. Empty container doesn't consump any pixel
     }
